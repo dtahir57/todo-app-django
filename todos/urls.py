e9e88a2f5b40c4dfeea2738d48a8f3259ab832f1
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-04-16 19:06:09 +0500

    Mark as undone

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addtask', views.addTask, name="addTask")
+    path('addtask', views.addTask, name="addTask"),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name="mark_as_done"),
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name="mark_as_undone")
 ]
