b13ca8175d4ba131b83b338b7e6b7d2d439267e1
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-04-16 18:39:12 +0500

    Updated

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,9 +3,9 @@ from todos.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
-    uncompleted_tasks = Task.objects.filter(is_completed=True)
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
-        'uncompleted_tasks': uncompleted_tasks
+        'completed_tasks': completed_tasks
     }
     return render(request, 'home.html', context)
